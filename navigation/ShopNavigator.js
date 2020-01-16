73beaa5f9a8871fea734c6fa902d37508333ef5d
Scapigliata <scapigliataludwig@gmail.com>
2020-01-16 15:09:57 +0100

    Add bottomNavigation with 3 tab menu: `Home`, `Movies` and `Profile`.

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,30 +1,79 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Platform } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import MovieOverViewScreen from '../screens/shop/MovieOverViewScreen';
 import MovieDetailScreen from '../screens/shop/MovieDetailScreen';
 import Colors from '../constants/Colours';
+
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+const defaultNavigationConfig = {
+  defaultNavigationOptions: {
+    headerTitle: 'GoodWatch',
+    headerStyle: {
+      backgroundColor: Colors.primary,
+    },
+    headerBackTitle: 'Back',
+    headerBackTitleStyle: {
+      color: 'white',
+    },
+    headerTintColor: 'white',
+  },
+};
 
 const MovieNavigator = createStackNavigator(
   {
     MovieOverview: MovieOverViewScreen,
     MovieDetail: MovieDetailScreen
   },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-      },
-      headerTitleStyle: {
-        fontFamily: 'open-sans-bold'
-      },
-      headerBackTitleStyle: {
-        fontFamily: 'open-sans'
-      },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
-  }
+  defaultNavigationConfig
+  // {
+  //   defaultNavigationOptions: {
+  //     headerStyle: {
+  //       backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+  //     },
+  //     headerTitleStyle: {
+  //       fontFamily: 'open-sans-bold'
+  //     },
+  //     headerBackTitleStyle: {
+  //       fontFamily: 'open-sans'
+  //     },
+  //     headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+  //   }
+  // }
 );
 
-export default createAppContainer(MovieNavigator);
+const AppNavigator = createMaterialBottomTabNavigator(
+  {
+    Movies: {
+      screen: MovieNavigator,
+      navigationOptions: {
+        title: 'Movies',
+        tabBarIcon: ({ tintColor }) => <MaterialIcon name='restaurant-menu' size={22} color={tintColor} />,
+      },
+    },
+    Home: {
+      screen: MovieNavigator,
+      navigationOptions: {
+        title: 'Home',
+        tabBarIcon: ({ tintColor }) => <MaterialCommunityIcon name='map-marker' size={22} color={tintColor} />,
+      },
+    },
+    Profile: {
+      screen: MovieNavigator,
+      navigationOptions: {
+        title: 'Profile',
+        tabBarIcon: ({ tintColor }) => <MaterialIcon name='person' size={26} color={tintColor} />,
+      },
+    },
+  },
+  {
+    activeColor: Colors.primary,
+    barStyle: { backgroundColor: '#fff' },
+  },
+);
+
+export default createAppContainer(AppNavigator);
