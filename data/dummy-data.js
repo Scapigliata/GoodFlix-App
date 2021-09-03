43711819d746d921e5bd7f2e956b95094537debc
Scapigliata <scapigliataludwig@gmail.com>
2021-09-04 01:07:59 +0200

    Updated dummy data

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -5,8 +5,8 @@ const Movies = [
     'p1',
     'u1',
     'Joker',
-    'https://pmcvariety.files.wordpress.com/2013/04/joker.jpg?w=720',
-    'The film paints a dark image of reality in Gotham. Throughouot the film there are countless cases of doubling. From the recurance of clowns in cars to Arthur Flecks iterations of banging his head into panes of glass, repetition is something that not only characterizes madness but also continues to permeate throughout the film. Reality seemingly unravels, and as the film progresses his sanity deteriorates, as mirrored in the surrounding.',    
+    'https://i1.wp.com/www.thelacanianreviews.com/wp-content/uploads/2019/10/joker.jpeg?fit=1280%2C720&ssl=1',
+    'The film paints a dark image of reality in Gotham. Throughouot the film there are countless cases of doubling. From the recurance of clowns in cars to Arthur Flecks iterations of banging his head into panes of glass, repetition is something that not only characterizes madness but also continues to permeate throughout the film. Reality seemingly unravels, and as the film progresses his sanity deteriorates, as mirrored in the surrounding.',
     '10/10'
   ),
   new Movie(
@@ -21,7 +21,7 @@ const Movies = [
     'p3',
     'u2',
     'Us',
-    'https://thoughtcatalog.files.wordpress.com/2019/04/mv5bztlinwjhm2ytndc1mc00ytk1lwe2mgytzme4m2y5oddlnzqzxkeyxkfqcgdeqxvymzy0mte3nzu40._v1_.jpg?w=1895&h=1280&crop=1&resize=1895,1280&quality=95&strip=all',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ1i67OvrxZCiRm8tdm40-VqiHeV5unBOipA&usqp=CAU',
     'Completely riveting!',
     '9/10'
   ),
@@ -30,7 +30,7 @@ const Movies = [
     'u3',
     'La Vita e bella',
     'https://cranforddialogue.com/wp-content/uploads/2018/05/LIFEISBEAUTIFUL_thumb_V1_FINAL_150320_webthumb_640x360_640x360_435875907539.jpg',
-    "Hilarious yet devastatingly morose tale of love and persecution during WWII",
+    'Hilarious yet devastatingly morose tale of love and persecution during WWII',
     '10/10'
   ),
   new Movie(
@@ -46,9 +46,9 @@ const Movies = [
     'u1',
     'Oblivion',
     'http://insessionfilm.com/wp-content/uploads/2013/02/Oblivion_movie-1024x589.jpg',
-    "Incredible cinematic experience, with an absolut giant of a soundtrack.",
+    'Incredible cinematic experience, with an absolut giant of a soundtrack.',
     '8/10'
-  )
+  ),
 ];
 
 export default Movies;
